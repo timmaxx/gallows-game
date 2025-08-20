5aa6b67e356bbb832f6e5f7a759e6e16a21e0335
timmaxx <m.n.timchenko@gmail.com>
2025-08-20 23:59:19 +0300

    realize multi rounds

--- src/main/java/com/timmax/game/gallows/Game.java
+++ src/main/java/com/timmax/game/gallows/Game.java
@@ -5,48 +5,58 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Game {
-    private final String word;
-    private final int maximumAttemptsNumber = 3;
     private static final Scanner scanner = new Scanner(System.in);
 
-    public Game(String word) {
-        this.word = word;
-    }
+    private static final int MAXIMUM_ATTEMPTS_NUMBER = 3;
 
     public void gameLoop() {
-        int currentAttemptsNumber = maximumAttemptsNumber;
-        Set<Character> openedLetters = new HashSet<>();
+        String word;
+        int currentAttemptsNumber;
+        Set<Character> openedLetters;
 
-        while (currentAttemptsNumber > 0) {
-            StringBuilder currentWord = new StringBuilder();
-            //  Цикл по слову
-            for (int i = 0; i < word.length(); i++) {
-                if (openedLetters.contains(word.charAt(i))) {
-                    currentWord.append(word.charAt(i));
-                } else {
-                    currentWord.append('*');
+        while (true) {
+            word = "asdf";
+            currentAttemptsNumber = MAXIMUM_ATTEMPTS_NUMBER;
+            openedLetters = new HashSet<>();
+            System.out.println("New round");
+
+            while (currentAttemptsNumber > 0) {
+                StringBuilder currentWord = new StringBuilder();
+                for (int i = 0; i < word.length(); i++) {
+                    if (openedLetters.contains(word.charAt(i))) {
+                        currentWord.append(word.charAt(i));
+                    } else {
+                        currentWord.append('*');
+                    }
                 }
+
+                System.out.println("Entered letters: " + openedLetters);
+                System.out.println("Current word: " + currentWord);
+                if (!currentWord.toString().contains(String.valueOf('*'))) {
+                    break;
+                }
+
+                System.out.println("You have " + currentAttemptsNumber + " attempts.");
+                System.out.println("Enter one letter");
+                char letter = scanner.nextLine().charAt(0);
+                if (!word.contains(String.valueOf(letter)) && !openedLetters.contains(letter)) {
+                    currentAttemptsNumber--;
+                }
+                openedLetters.add(letter);
+            }
+            if (currentAttemptsNumber == 0) {
+                System.out.println("You have not any attempts!");
+                System.out.println("You lost!");
+            } else {
+                System.out.println("You win!");
             }
 
-            System.out.println("Entered letters: " + openedLetters);
-            System.out.println("Current word: " + currentWord);
-            if(!currentWord.toString().contains(String.valueOf('*'))) {
+            System.out.println("If you want to exit game, enter Q. Else you will begin next round.");
+
+            String answer = scanner.nextLine();
+            if (answer.equals("Q")) {
                 break;
             }
-
-            System.out.println("You have " + currentAttemptsNumber + " attempts.");
-            System.out.println("Enter one letter");
-            char letter = scanner.nextLine().charAt(0);
-            if(!word.contains(String.valueOf(letter)) && !openedLetters.contains(letter)) {
-                currentAttemptsNumber--;
-            }
-            openedLetters.add(letter);
-        }
-        if (currentAttemptsNumber == 0) {
-            System.out.println("You have not any attempts!");
-            System.out.println("You lost!");
-        } else {
-            System.out.println("You win!");
         }
     }
 }
--- src/main/java/com/timmax/game/gallows/Main.java
+++ src/main/java/com/timmax/game/gallows/Main.java
@@ -3,7 +3,7 @@ package com.timmax.game.gallows;
 public class Main {
     public static void main(String[] args) {
 
-        Game game = new Game("фыва");
+        Game game = new Game();
         game.gameLoop();
     }
 }
